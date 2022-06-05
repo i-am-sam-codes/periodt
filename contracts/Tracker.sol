a368c5fd3055e9f87704f6b5a0d42bec99eb419a
i-am-sam-codes <sammmceja@gmail.com>
2022-06-04 21:02:41 -0700

    total days

--- contracts/Tracker.sol
+++ contracts/Tracker.sol
@@ -5,7 +5,19 @@ pragma solidity ^0.8.0;
 import "hardhat/console.sol";
 
 contract Tracker {
+    uint256 totalDays;
+
     constructor() {
         console.log("Yooooooooooo");
+    }
+
+    function totaldays() public {
+        totalDays += 1;
+        console.log("%s thanks for tracking your flow!", msg.sender);
+    }
+
+    function getTotalDays() public view returns (uint256) {
+        console.log("You've had your flow for %d days!", totalDays);
+        return totalDays;
     }
 }
--- scripts/run.js
+++ scripts/run.js
@@ -1,8 +1,20 @@
 const main = async () => {
+    const [owner, randomPerson] = await hre.ethers.getSigners();
     const trackerContractFactory = await hre.ethers.getContractFactory("Tracker");
     const trackerContract = await trackerContractFactory.deploy();
     await trackerContract.deployed();
+
     console.log("Contract deployed to:", trackerContract.address);
+
+    console.log("Contract deployed by:", owner.address);
+
+    let dayCount;
+    dayCount = await trackerContract.getTotalDays();
+  
+    let dayTxn = await trackerContract.totaldays();
+    await dayTxn.wait();
+  
+    dayCount = await trackerContract.getTotalDays();
 };
 
 const runMain = async () => {
